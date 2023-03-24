6ce571cd470f01847504f7cbf27b2a05fe9ee9cf
TomJGooding <101601846+TomJGooding@users.noreply.github.com>
2023-03-24 21:13:31 +0000

    refactor(app): move location search to widget

--- src/weather_uk/app/screens/locations.py
+++ src/weather_uk/app/screens/locations.py
@@ -1,11 +1,9 @@
-import requests
 from textual.app import ComposeResult
 from textual.containers import Container
 from textual.screen import Screen
 from textual.widgets import Footer, Input, Markdown
-from textual_autocomplete import AutoComplete, Dropdown, DropdownItem
 
-from weather_uk.locations import services
+from weather_uk.app.widgets.locations import LocationSearch
 
 
 class LocationsScreen(Screen):
@@ -15,30 +13,7 @@ class LocationsScreen(Screen):
     def compose(self) -> ComposeResult:
         yield Container(
             Markdown("# Find a forecast"),
-            AutoComplete(
-                Input(placeholder="Search for a location"),
-                Dropdown(items=self.get_location_items()),
-            ),
+            LocationSearch(),
             classes=("center-box"),
         )
         yield Footer()
-
-    def get_location_items(self) -> list[DropdownItem]:
-        weather_api = self.app._weather_api  # type: ignore[attr-defined]
-        locations_data = []
-        try:
-            locations_data = services.get_locations_list(weather_api)
-
-        # TODO: Handle requests exceptions
-        except requests.exceptions.HTTPError:
-            pass
-
-        return [
-            DropdownItem(main=str(location), right_meta=str(location.id))
-            for location in locations_data
-        ]
-
-    def on_auto_complete_selected(self, event: AutoComplete.Selected) -> None:
-        location_id: int = int(str(event.item.right_meta))
-        self.app._location_id = location_id  # type: ignore[attr-defined]
-        self.app.push_screen("forecast")
